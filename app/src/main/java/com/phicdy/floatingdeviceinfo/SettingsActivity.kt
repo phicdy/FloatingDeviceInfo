84e73b845effa56ec830b9c060e18de8ebd8af4e
phicdy <phicdy@gmail.com>
2018-02-12 14:35:36 +0900

    UI: Delete unneeded default option in place

--- app/src/main/java/com/phicdy/floatingdeviceinfo/SettingsActivity.kt
+++ app/src/main/java/com/phicdy/floatingdeviceinfo/SettingsActivity.kt
@@ -83,7 +83,6 @@ class SettingsActivity : AppCompatPreferenceActivity() {
             // to their values. When their values change, their summaries are
             // updated to reflect the new value, per the Android Design
             // guidelines.
-            bindPreferenceSummaryToValue(findPreference("example_text"))
             bindPreferenceSummaryToValue(findPreference("example_list"))
         }
 
